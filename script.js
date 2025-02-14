465314ece9ee0b1885799a0fad486c76d01b15a7
mapa5 <90824681+mapa5@users.noreply.github.com>
2025-02-14 16:34:56 +0100

    fin du script de plat

--- script.js
+++ script.js
@@ -5,96 +5,100 @@ fetch("https://www.themealdb.com/api/json/v1/1/random.php")
         console.log(data.meals[0].idMeal);
 
 
+        // <div class="plat">
+        //    <h2></h2>
+        //    <figure class='pays'>
+        //        <img src="">
+        //        <figcaption></figcaption>
+        //    </figure>
+        //    <figure>
+        //        <img src="">
+        //        <figcaption></figcaption>
+        //    </figure>
+        //    <div class="ingr">
+        //        <img src="">
+        //        <span></span>
+        //        <span></span>
+        //    </div>
+        //    <span></span>
+        //    <video></video>
+        // </div>
+
+        const sectionplat = document.querySelector(".random");
+        let divplat = document.createElement("div");
+        divplat.className = 'plat'
+        sectionplat.appendChild(divplat);
+
+        const plat = document.querySelector(".plat");
 
 
         for (const elem in data.meals[0]) {
+
             console.log(`${elem}: ${data.meals[0][elem]}`);
-            const sectionplat = document.querySelector(".random");
-            let divplat = document.createElement("div");
-            divplat.className = 'plat'
-            sectionplat.appendChild(divplat);
-            const plat = document.querySelector(".plat");
+
+
 
             switch (elem) {
                 case "strMeal":
-                    let diving = document.createElement("span");
-                    diving.className = 'ingr'
-                    const divseling = document.querySelector("");
-                    divseling.appendChild(diving);
-
+                    {
+                        let nomplat = document.createElement("h2");
+                        nomplat.textContent = data.meals[0].strMeal
+                        plat.appendChild(nomplat);
+                        continue
+                    }
                 case "strArea":
-                    let = document.createElement("span");
-                    .textContent = data.meals[0][`strMeasure${i}`];
-                    const = document.querySelector(".ingr");
-                    .appendChild();
-
+                    {
+                        let descpays = document.createElement("span");
+                        descpays.textContent = data.meals[0][`strMeasure${i}`];
+                        plat.appendChild(descpays);
+                        continue
+                    }
                 case "strInstructions":
-                    let = document.createElement("span");
-                    .textContent = data.meals[0][`strMeasure${i}`];
-                    const = document.querySelector(".ingr");
-                    .appendChild();
+                    {
 
+                        let recette = document.createElement("span");
+                        recette.textContent = data.meals[0].strInstructions;
+                        plat.appendChild(recette);
+                        continue
+                    }
                 case "strMealThumb":
-                    let = document.createElement("span");
-                    .textContent = data.meals[0][`strMeasure${i}`];
-                    const = document.querySelector(".ingr");
-                    .appendChild();
+                    {
 
+                        let thumbplat = document.createElement("img");
+                        thumbplat.src = data.meals[0].strMealThumb;
+                        plat.appendChild(thumbplat);
+                        continue
+                    }
                 case "strYoutube":
-                    let = document.createElement("span");
-                    .textContent = data.meals[0][`strMeasure${i}`];
-                    const = document.querySelector(".ingr");
-                    .appendChild();
+                    {
+                        let ytlink = document.createElement("video");
+                        ytlink.src = data.meals[0].strYoutube;
+                        plat.appendChild(ytlink);
+                        continue
+                    }
             }
         }
         for (var i = 1; i <= 20; i++) {
 
             if (data.meals[0][`strIngredient${i}`] != "") {
                 let divingr = document.createElement("div");
-                divingr.className = 'ingr'
+                divingr.className = `ingr${i}`
                 plat.appendChild(divingr);
-
-                const ingr = document.querySelector(".ingr");
-
+                const ingr = document.querySelector(`.ingr${i}`);
                 let imgingredient = document.createElement("img");
                 let ingredient = document.createElement("span");
                 let mesure = document.createElement("span");
-
                 imgingredient.src = `https://www.themealdb.com/images/ingredients/${data.meals[0][`strIngredient${i}`]}-Small.png`;
                 ingredient.textContent = data.meals[0][`strIngredient${i}`];
                 mesure.textContent = data.meals[0][`strMeasure${i}`];;
-
                 ingr.appendChild(imgingredient);
                 ingr.appendChild(ingredient);
                 ingr.appendChild(mesure);
-
             } else
                 break
         }
     })
+
     .catch((err) => {
         console.error(err);
     });
-
-
-
-
-
-// <div class="plat">
-//    <h2></h2>
-//    <figure>
-//        <img src="">
-//        <figcaption></figcaption>
-//    </figure>
-//    <figure>
-//        <img src="">
-//        <figcaption></figcaption>
-//    </figure>
-//    <div class="ingr">
-//        <img src="">
-//        <span></span>
-//        <span></span>
-//    </div>
-//    <span></span>
-//    <video></video>
-// </div>
